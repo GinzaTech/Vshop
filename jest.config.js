b58545e47923e8f1ceaab31d65904602bb0da6b7
GinzaTech <thanhloc150204@gmail.com>
2026-08-07 00:57:25 +0700

    test: configure jest coverage checks

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "jest-expo",
+  clearMocks: true,
+  roots: ["<rootDir>/__tests__"],
+  testMatch: ["**/*.test.[jt]s?(x)"],
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/$1",
+  },
+  collectCoverageFrom: ["utils/session-events.ts"],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/backup/",
+    "/.codex-tmp/",
+    "/rn-flow-visualizer/",
+    "/valorant-api-docs/",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
